03c8dfb9b1147940be7927a56c6ea8051970e840
Paras Roy <93031750+Paras-Roy@users.noreply.github.com>
2023-03-09 14:40:06 +0530

    commit check

--- commands/paid.py
+++ commands/paid.py
@@ -1,5 +1,5 @@
 # Sample Input - (comment)
-# comment.body = "$confirm /u/chiefwinamac 10"
+# comment.body = "$confirm /u/chiefwinamac 10" 
 '''
 /u/chiefwinamac indicated /u/Sad-Quit-303 repaid him 10.00 USD.
 

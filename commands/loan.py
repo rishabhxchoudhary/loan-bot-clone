8cb8b1fb114b725f3c002c8c4b35db203ab33455
Ansh Tanwar <97782299+AnshTanwar@users.noreply.github.com>
2023-03-10 03:45:58 +0530

    loan_logic_updated

--- commands/loan.py
+++ commands/loan.py
@@ -12,44 +12,57 @@ import re
 #mongo_collection="transactions"
 #collection = pymongo.MongoClient(mongo_uri)[mongo_dbname][mongo_collection]
 
-
-def loan(comment):
+def loan(self,comment):
 
         print("loan")
+        post = comment.submission
+        post_url = post.url
+
+        #get the loan details from database with 'Orignal Thread' equal to post_url
+        myquery = {'Orignal Thread': post_url}
+        doc = self.collection.find(myquery)
+
+        #get comment details
         loan_command = comment.body.split()[0]    #$loan
         loan_amount_given = int(comment.body.split()[1])  #10
         loan_amount_max_asked = int(re.search(r'\((.*?)\)',comment.submission.title).group(1))
         
         lender_name = comment.author.name 
-        Borrower_name = comment.submission.author
-        paid_with_id  = "141089"
+        borrower_name = comment.submission.author
+        paid_with_id  = "XXXXX"
 
-        if (loan_command == "!loan") and (loan_amount_given<=loan_amount_max_asked) :
+        if loan_amount_given<=loan_amount_max_asked and loan_amount_given>0:
             global lended #BOOL CHECK
             lended = True
 
             highlighted_text_1 = "!confirm {} {} USD".format(lender_name, loan_amount_given)
             highlighted_text_2 = "!paid_with_id {} {} USD".format(paid_with_id, loan_amount_given)
-
+            
+            
             # Construct the message using f-strings
-            message = f"Noted! I will remember that {lender_name} lent {loan_amount_given} USD to {Borrower_name}\n\n" \
+            message = f"Noted! I will remember that [{lender_name}](/u/{lender_name}) lent {loan_amount_given} USD to [{borrower_name}](/u/{borrower_name})\n\n" \
             f"The format of the confirm command will be:\n"\
             f"""
             {highlighted_text_1}""" \
             f"\n\nIf you wish to mark this loan repaid later, you can use:\n"\
             f"""
             {highlighted_text_2}""" \
-            f"\n\nThis does NOT verify that {lender_name} actually lent anything to {Borrower_name}; " \
-            f"{Borrower_name} should confirm here or nearby that the money was sent" \
-            f"\n\nIf the loan transaction did not work out and needs to be refunded then the lender should " \
-            f"reply to this comment with 'Refunded' and moderators will be automatically notified"
-
+            f"\n\n  "\
+            f"\n\nThis does NOT verify that [{lender_name}](/u/{lender_name}) actually lent anything to [{borrower_name}](/u/{borrower_name});\n\n " \
+            f"[{borrower_name}](/u/{borrower_name}) should confirm here or nearby that the money was sent" \
+            f"\n\n**If the loan transaction did not work out and needs to be refunded then the lender should" \
+            f"reply to this comment with 'Refunded' and moderators will be automatically notified**"
+            
             comment.reply(message)
+            myquery = {'Orignal Thread': post_url}
+            newvalues = { "$set": { "Lender":lender_name, "Amount Given":loan_amount_given } }
+            self.collection.update_one(myquery, newvalues)
 
-        elif loan_amount_given > loan_amount_max_asked:
+
+        else:
               message = f"Maximum Amount you can Lend is {loan_amount_max_asked} $"
               comment.reply(message)
-              
+
 
 
 

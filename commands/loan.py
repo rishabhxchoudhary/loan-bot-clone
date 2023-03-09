f4ebc9bd6006a4a3ea11efde1a5cfe9ff2529bc0
Ansh Tanwar <97782299+AnshTanwar@users.noreply.github.com>
2023-03-09 23:26:37 +0530

    Loan function completed
    lended set to true by default

--- commands/loan.py
+++ commands/loan.py
@@ -1,70 +1,55 @@
 # Sample Input - (comment)
 # comment.body = "$loan 10"
+#amount requested check
+
 
 # Sample output
-import pymongo
-from create_table_from_list import create_table_from_list
-import re
 
+import pymongo
+import re
 mongo_uri="mongodb+srv://root:root@cluster.if4vaf9.mongodb.net/?retryWrites=true&w=majority"
 mongo_dbname="reddit"
 mongo_collection="transactions"
 collection = pymongo.MongoClient(mongo_uri)[mongo_dbname][mongo_collection]
 
-def add_lender_record(comment):
-        data = {'id': comment.id,
-                'borrower': "",
-                'lender': comment.author.name,
-                'Amount Given': 0,
-                'Amount Repaid': 0,
-                'Original Thread':"",
-                'Date Given': "",
-                'Date Paid Back': "",
-                }
-        collection.insert_one(data)
-        #message = f"You request has been opened with ID : {comment.id}"
-        #comment.reply(message)
-
 
 def loan(comment):
-        #check for previous lender records
-        id_lender = comment.body.split()[1]
-        myquery = {'id': id}
-        doc = collection.find_one(myquery)
-        if doc["Lender"] == comment.author.name:
-             #Record already exists
-             pass
-        else:
-             add_lender_record(comment)
 
-   
-        comment_body_command = comment.body.split()[0]    #$loan
-        loan_amount = comment.body.split()[1]  #10
-        lender_name = comment.author.name     # "/u/chiefwinamac"
-        Borrower_name = "/u/Sad-Quit-303"
+        print("loan")
+        loan_command = comment.body.split()[0]    #$loan
+        loan_amount_given = int(comment.body.split()[1])  #10
+        loan_amount_max_asked = int(re.search(r'\((.*?)\)',comment.submission.title).group(1))
+        
+        lender_name = comment.author.name 
+        Borrower_name = comment.submission.author
         paid_with_id  = "141089"
 
-        if comment_body_command == "$loan":
-            message = f"""Noted! I will remember that {lender_name} lent {loan_amount} USD to {Borrower_name}
+        if (loan_command == "!loan") and (loan_amount_given<=loan_amount_max_asked) :
+            global lended #BOOL CHECK
+            lended = True
 
-            The format of the confirm command will be:
+            highlighted_text_1 = "!confirm {} {} USD".format(lender_name, loan_amount_given)
+            highlighted_text_2 = "!paid_with_id {} {} USD".format(paid_with_id, loan_amount_given)
 
-            $confirm {lender_name} {loan_amount} USD
+            # Construct the message using f-strings
+            message = f"Noted! I will remember that {lender_name} lent {loan_amount_given} USD to {Borrower_name}\n\n" \
+            f"The format of the confirm command will be:\n"\
+            f"""
+            {highlighted_text_1}""" \
+            f"\n\nIf you wish to mark this loan repaid later, you can use:\n"\
+            f"""
+            {highlighted_text_2}""" \
+            f"\n\nThis does NOT verify that {lender_name} actually lent anything to {Borrower_name}; " \
+            f"{Borrower_name} should confirm here or nearby that the money was sent" \
+            f"\n\nIf the loan transaction did not work out and needs to be refunded then the lender should " \
+            f"reply to this comment with 'Refunded' and moderators will be automatically notified"
 
-            If you wish to mark this loan repaid later, you can use:
-
-            $paid_with_id {paid_with_id} {loan_amount} USD
-
-            This does NOT verify that {lender_name} actually lent anything to {Borrower_name}; {Borrower_name} should confirm here or nearby that the money was sent
-
-            If the loan transaction did not work out and needs to be refunded then the lender should reply to this comment with 'Refunded' and moderators will be automatically notified
-
-            """
             comment.reply(message)
-        else :
-              #other commands
-              #error/typo
-              pass
+
+        elif loan_amount_given > loan_amount_max_asked:
+              message = f"Maximum Amount you can Lend is {loan_amount_max_asked} $"
+              comment.reply(message)
+              
 
 
 

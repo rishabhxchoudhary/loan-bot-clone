66aaa0fa754072fea95d5539a5c4dab14949a9e9
Rishabh Kumar Choudhary <97455703+rk26072003@users.noreply.github.com>
2023-06-03 17:34:34 +0530

    final

--- credentials.py
+++ credentials.py
@@ -1,9 +1,11 @@
-username = "Fit-Belt1935"
-password = "Rishabh@12345"
-client_id = "H2f8VSpd-8t5AoQSGZVVoA"
-client_secret='c5PexU5V63uQYOGeMQWAz0pqG1FN0w'
-user_agent="Reddit bot for bidding"
-mongo_uri="mongodb+srv://root:root@cluster.if4vaf9.mongodb.net/?retryWrites=true&w=majority"
-mongo_dbname="reddit"
-mongo_collection="transactions"
-subreddit_name = "rktestingbtot"
+username = ""  # username of reddit account
+password = ""  # password of reddit account
+client_id = ""  # client id of reddit app
+client_secret = ""  # client secret of reddit app
+user_agent = "Reddit bot for bidding"
+
+
+mongo_uri = ""
+mongo_dbname = "reddit"
+mongo_collection = "transactions"
+subreddit_name = "rktestingbtot"  # subreddit name
